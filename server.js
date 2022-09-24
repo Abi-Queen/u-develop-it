b571a845b691e9eb27d6e37d32b706db15f0cd05
Abigail Queen <107533064+Abi-Queen@users.noreply.github.com>
2022-09-24 04:44:26 -0700

    checked connection with db.query

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 //import express
 const express = require('express');
+//import mysql
+const mysql = require('mysql2');
+
 //add port designation and app expression 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -8,8 +11,25 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json()); 
 
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //your mysql username,
+        user: 'root',
+        //your mysql password
+        password: 'Outvilejelly123',
+        database: 'election'
+    },
+    console.log('connected to election database')
+);
 
-//default response for any other request (not found)
+// test connection with query 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
+//default response for any other request (not found); must place the default error route BELOW all other routes or it will override them
 app.use((req, res) => {
     res.status(404).end();
 });
